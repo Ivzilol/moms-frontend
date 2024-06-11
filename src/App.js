c88c496cbcf93eaba723d84097d36edd17742ef1
fondle <semovagalina@gmail.com>
2024-06-11 18:34:54 +0300

    some adjustments in projects structure, playwright, bootstrap installed, react-router-dom version changed to @6 and Switch is replased with Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import OrderPage from './pages/OrderPage';
-import NotFoundPage from './pages/NotFoundPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/login/LoginPage';
+import RegisterPage from './pages/register/RegisterPage';
+import OrderPage from './pages/orderPage/OrderPage';
+import NotFoundPage from './pages/404/NotFoundPage';
 import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
   return (
       <AuthProvider>
         <Router>
-          <Switch>
+          <Routes>
             <Route exact path="/" component={HomePage} />
             <Route path="/login" component={LoginPage} />
             <Route path="/register" component={RegisterPage} />
             <Route path="/orders" component={OrderPage} />
             <Route component={NotFoundPage} />
-          </Switch>
+          </Routes>
         </Router>
       </AuthProvider>
   );
---
+++ tests/playwright.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testDir: './tests',
+    use: {
+      headless: true,
+      baseURL: 'http://localhost:3000', 
+    },
+  };
