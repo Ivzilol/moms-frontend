9cd862fcd4cc8353de9ead006cb3f959791aab96
fondle <semovagalina@gmail.com>
2024-06-25 15:10:31 +0300

    profile card created and linked to the main SideMenu

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './SideMenu.module.css';
 
 import TopTabsNavUsers from '../topTabsNav/TopTabsNavUsers';
+import ProfileCard from '../profile/ProfileCard';
 
 
 const SideMenu = () => {
@@ -12,12 +13,12 @@ const SideMenu = () => {
         <button className={`nav-link active ${classes.navLink}`} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Създай Поръчки</button>
         <button className={`nav-link ${classes.navLink}`} id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Моите Поръчки</button>
         <button className={`nav-link ${classes.navLink}`} id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Профил</button>
-        <button className={`nav-link ${classes.navLink}`} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Създай Нов Акаунт</button>
+        <button className={`nav-link ${classes.navLink}`} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Акаунти</button>
       </div>
       <div className={`tab-content ${classes.tabContent}`} id="v-pills-tabContent">
         <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabIndex="0">Home content...</div>
         <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabIndex="0">Order content....</div>
-        <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabIndex="0">Messages content...</div>
+        <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabIndex="0"><ProfileCard/></div>
         <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabIndex="0"><TopTabsNavUsers/></div>
       </div>
     </div>
