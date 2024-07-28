94eb498eb66d6d6d32062beee38546e86c01cd76
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-07-28 13:42:39 +0300

    create order

--- src/components/createOrder/EditItemModal.js
+++ src/components/createOrder/EditItemModal.js
@@ -41,10 +41,10 @@ const EditItemModal = ({ item, onSave, onClose }) => {
                 </label>
                 <label>
                     м. ед.:
-                    <select value={editedItem.lengthUnit} onChange={handleChange}>
-                        <option value="MM">ММ</option>
-                        <option value="CM">СМ</option>
-                        <option value="M">М</option>
+                    <select name="lengthUnit" value={editedItem.lengthUnit} onChange={handleChange}>
+                        <option value="mm">mm</option>
+                        <option value="cm">cm</option>
+                        <option value="m">m</option>
                     </select>
                 </label>
                 <label>
