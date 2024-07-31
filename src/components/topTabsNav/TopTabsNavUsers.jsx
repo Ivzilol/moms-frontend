1a0a274f1c5a91c2fe22015bfc3f86fa1dfd5751
fondle <semovagalina@gmail.com>
2024-07-31 12:08:25 +0300

    on successfull message added after redirection

--- src/components/topTabsNav/TopTabsNavUsers.jsx
+++ src/components/topTabsNav/TopTabsNavUsers.jsx
@@ -11,10 +11,18 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 const TopTabsNav = () => {
   
   const [activeTab, setActiveTab] = useState('v-pills-home');
+  const [successMessage, setSuccessMessage] = useState('');
 
- 
   const handleTabClick = (tabId) => {
     setActiveTab(tabId);
+    if (tabId !== 'v-pills-home') {
+      setSuccessMessage(''); 
+    }
+  };
+
+  const handleRegistrationSuccess = () => {
+    setActiveTab('v-pills-home');
+    setSuccessMessage('Регистрацията е успешна!');
   };
 
   return (
@@ -53,10 +61,15 @@ const TopTabsNav = () => {
 
         <div className={`tab-pane fade ${activeTab === 'v-pills-new' ? 'show active' : ''}`} id="v-pills-new" role="tabpanel" aria-labelledby="v-pills-home-tab">
           <h3>Нов Акаунт</h3>
-          <p><RegisterForm onSuccess={() => handleTabClick('v-pills-home')} /></p>
+          <p><RegisterForm onSuccess={handleRegistrationSuccess} /></p>
         </div>
         <div className={`tab-pane fade ${activeTab === 'v-pills-home' ? 'show active' : ''}`} id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
           <h3>Всички</h3>
+          {successMessage && (
+            <div className={`alert alert-success ${styles.successContainer}`} role="alert">
+              {successMessage}
+            </div>
+          )}
           <p><UserList/></p>
         </div>
         <div className={`tab-pane fade ${activeTab === 'v-pills-profile' ? 'show active' : ''}`} id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
