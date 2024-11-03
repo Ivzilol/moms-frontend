c68cc066aad1c8868bf1b4ed5d3fe359feee680e
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-03 17:02:08 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
                            </PrivateRoute>
 
                    }/>
+
             <Route path="/create-construction-site"
                    element={
                        hasAdminSuperadminRole ?
