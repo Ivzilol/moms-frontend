018485bd30fea49f1083c4909ca9705bfc88afc2
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-20 21:11:07 +0200

    refactor

--- src/assets/core/environments/costants.js
+++ src/assets/core/environments/costants.js
@@ -1,6 +1,4 @@
 export const host = 'https://localhost:7172/api/';
-// replace with this on deploy to production
-// export const host = 'https://ai-social-platform.azurewebsites.net/api/';
 
 export const PATH = {
     home: '/',
