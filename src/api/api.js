a261bc10c93d3734d93cb4c50011fd94433249e3
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-20 21:10:18 +0200

    refactor

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,4 @@
-// import axios from 'axios';
 
-// const api = axios.create({
-//     baseURL: process.env.REACT_APP_API_URL,
-// });
-
-// api.interceptors.request.use((config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-// });
-
-// export default api;
 
 import { ContentType, tokenName, host } from '../core/environments/constants';
 
